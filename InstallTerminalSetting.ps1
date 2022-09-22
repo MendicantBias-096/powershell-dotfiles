35b59f3b411b6c00cefcab6cace5bf87e7cfed1d
Arturo M Jacome P <a.m.jacomep.test@gmail.com>
2022-09-22 01:07:35 -0500

    feat(ITS): Se modificaron los mensajes de instalación y algunos comandos con errores de escritura

--- InstallTerminalSetting.ps1
+++ InstallTerminalSetting.ps1
@@ -1,44 +1,73 @@
-"Instalamos el Gestor de paquetes Scope"
-
+"[ ----------------------------------------------]"
+"[ ==> Instalamos el Gestor de paquetes Scope <==]"
+"[ ----------------------------------------------]"
 Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-scoop isntall curl sudo jq
+scoop install curl sudo jq
 
-"Instalamos Git"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos Git <================]"
+"[ ----------------------------------------------]"
 winget install -e --id Git.Git
 
-"Instalamos NeoVim"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos NeoVim <=============]"
+"[ ----------------------------------------------]"
 scoop install neovim gcc
 
-"Instalamos posh-git"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos posh-git <===========]"
+"[ ----------------------------------------------]"
 Install-Module posh-git -Scope CurrentUser -Force
 
-"Instalamos Oh-my-posh"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos Oh-my-posh <=========]"
+"[ ----------------------------------------------]"
 Set-ExecutionPolicy Bypass -Scope Process -Force Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
-"Creamos la carpeta de powershell en la raiz de usuarios"
-mkdir .config/powershell
+"[ --------------------------------------------------------------------------------]"
+"[ ============> Creamos la carpeta de powershell en la raiz de usuarios <=========]"
+"[ --------------------------------------------------------------------------------]"
+mkdir \.config\powershell
 
-"Creamo la carpeta PowerShell en documentos"
-mkdir .\Documents\PowerShell
+"[ -------------------------------------------------------------------]"
+"[ ============> Creamo la carpeta PowerShell en documentos <=========]"
+"[ -------------------------------------------------------------------]"
+mkdir \Documents\PowerShell
 
-"Instalamos Node"
-scoope install nvm
+"[ ----------------------------------------------]"
+"[ ============> Instalamos Node <===============]"
+"[ ----------------------------------------------]"
+scoop install nvm
 
-"Instalamos el modulo de Iconos"
+"[ -------------------------------------------------------------]"
+"[ ============> Instalamos el modulo de Iconos <===============]"
+"[ -------------------------------------------------------------]"
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 
-"Instalamos z Directory"
+""
+"[ -----------------------------------------------------]"
+"[ ============> Instalamos z Directory <===============]"
+"[ -----------------------------------------------------]"
 Install-Module -Name z -Force
 
-"Instalamos PSReadLine"
+"[ ----------------------------------------------------]"
+"[ ============> Instalamos PSReadLine <===============]"
+"[ ----------------------------------------------------]"
+""
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 
-"Instalamos fzf"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos fzf <================]"
+"[ ----------------------------------------------]"
 scoop isntall fzf
 
-"Instalamos PSFzf"
+"[ ------------------------------------------------]"
+"[ ============> Instalamos PSFzf <================]"
+"[ ------------------------------------------------]"
 Install-Module -Name PSFzf -Scope CurrentUser -Force
 
-"Instalamos bat"
+"[ ----------------------------------------------]"
+"[ ============> Instalamos bat <================]"
+"[ ----------------------------------------------]"
 scoop install bat
 
